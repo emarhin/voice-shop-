ca9d220d4f553dda09423af8b0fbdb2bc8faf0d7
Emmanuel Arhin <dontstop.ea@gmail.com>
2022-10-21 19:03:59 +0000

    created django apps and added order and  product models

---
+++ backend/shop/models.py
@@ -0,0 +1,16 @@
+from itertools import product
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+
+class product(models.Model):
+    name = models.CharField(max_length=50,null=False ,blank = False)
+    description = models.CharField(max_length=500 , null= False ,blank=False )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+class order(models.Model):
+    product = models.ForeignKey(product,related_name='product',on_delete=models.PROTECT ,blank=True, null=True)
+    location_to = models.CharField(max_length=50,null=False ,blank = False)
+    product_at =   models.CharField(max_length=50,null=False ,blank = False)
+    phoneNumber = models.CharField(max_length=50,null=False ,blank = False)
